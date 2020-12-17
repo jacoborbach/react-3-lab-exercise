e3486b601dc8153828fb5d1ec26788194e4aef91
jacoborbach <jacoborbach96@gmail.com>
2020-12-17 16:42:33 -0500

    latest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import { Component } from 'react';
 import Header from './Components/Header';
 import './App.css';
+import DisplayPerson from './Components/DisplayPerson';
 
-function App() {
-  return (
-    <section className="App">
-      <Header />
-      <body className="body">
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
 
-      </body>
-    </section>
-  );
+    }
+  }
+
+  previousPerson = () => {
+
+  }
+
+  nextPerson = () => {
+    // invoke a display of data
+    // call a function or render a component that would go to next person on array
+  }
+
+  render() {
+    return (
+      <section className="App" >
+        <Header />
+        <body className="body">
+          <DisplayPerson />
+          <button onclick={this.previousPerson}>Previous</button>
+          <button onclick={this.nextPerson}>Next</button>
+        </body>
+      </section>
+    );
+  }
 }
 
 export default App;
