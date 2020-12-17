d20156ea97116dc0dbc796836f45f883efce41b1
jacoborbach <jacoborbach96@gmail.com>
2020-12-17 16:00:59 -0500

    first draft

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='headerContent'>Home</div>
+        </header>
+    )
+}
+
+export default Header;
